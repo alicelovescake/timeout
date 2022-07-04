d015e615efbc8d1dcae0dd9b488773a0344122a5
Anthony Morris <amorriscode@gmail.com>
2022-07-03 22:12:37 -0700

    chore: tighten design

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -21,30 +21,55 @@ export default function Index() {
   const { user, timeouts } = useLoaderData<LoaderData>()
 
   return (
-    <div className="max-w-4xl mx-auto py-8 space-y-8 font-mono">
+    <div className="max-w-4xl mx-auto py-8 font-mono">
       <div className="text-center">
-        <h1 className="uppercase tracking-widest font-bold text-8xl">
-          timeout
-        </h1>
+        <div className="relative flex justify-center items-center">
+          <div className="uppercase font-bold text-8xl py-2 px-4 space-x-4 border-2 border-black">
+            <span>t</span>
+            <span>i</span>
+            <span>m</span>
+            <span>e</span>
+          </div>
+          <div className="uppercase font-bold text-8xl text-brand-white bg-black border-2 border-black py-2 px-4 space-x-4">
+            <span>0</span>
+            <span>u</span>
+            <span>t</span>
+          </div>
+        </div>
 
-        <p className="font-bold">
+        <p className="font-bold mt-2">
           Temporarily ban or mute the worst of Twitter.
         </p>
       </div>
 
-      <div>
+      <div className="mt-12">
         {user ? (
           <>
-            <div className="border border-black rounded-xl p-8 space-y-8">
+            <div className="border-2 border-black p-8 space-y-8">
               <div className="flex space-x-4">
-                <img
-                  src={user.profileImageURL}
-                  alt="profile-pic"
-                  className="rounded-xl border border-black w-12 h-12"
-                />
+                <div className="relative w-12 h-12">
+                  <img
+                    src={user.profileImageURL}
+                    alt="profile-pic"
+                    className="border-2 border-black w-12 h-12 absolute -ml-0.5 -mt-0.5 z-10"
+                  />
+                  <div className="bg-black w-12 h-12 absolute"></div>
+                </div>
 
                 <div>
-                  <h2>Howdy {user.name}, let's put some folks in a timeout!</h2>
+                  <p>
+                    Howdy{' '}
+                    <a
+                      href={`https://twitter.com/${user.handle}`}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="font-bold hover:line-through decoration-2"
+                    >
+                      @{user.handle}
+                    </a>
+                    .
+                  </p>
+                  <p>You've put {timeouts.length} people in a timeout.</p>
                 </div>
               </div>
 
@@ -55,18 +80,30 @@ export default function Index() {
               )}
             </div>
 
-            <Form method="post" action="/logout" className="text-right mt-2">
-              <button>Log out</button>
+            <Form
+              method="post"
+              action="/logout"
+              className="flex justify-end items-center"
+            >
+              <button className="p-2 font-bold hover:line-through decoration-2">
+                Log out
+              </button>
             </Form>
           </>
         ) : (
-          <div className="flex justify-center">
-            <Form method="post" action="/login">
-              <button className="hover:bg-brand-blue hover:-mt-2 font-bold p-8 rounded-xl border border-black">
-                Login with Twitter
-              </button>
-            </Form>
-          </div>
+          <Form
+            method="post"
+            action="/login"
+            className="relative flex justify-center"
+          >
+            <button className="absolute w-max z-10 bg-brand-white -ml-2.5 -mt-2 hover:-ml-3 hover:-mt-2.5 font-bold p-4 border-2 border-black transition-all ease-in-out">
+              Log in with Twitter
+            </button>
+
+            <div className="absolute bg-black p-4 w-max transition-colors ease-in-out">
+              Log in with Twitter
+            </div>
+          </Form>
         )}
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,6 @@ module.exports = {
       colors: {
         brand: {
           white: '#EBDED1',
-          brown: '#CAAA95',
-          red: '#926E6F',
-          purple: '#564D78',
-          blue: '#9ACBD0',
         },
       },
     },
